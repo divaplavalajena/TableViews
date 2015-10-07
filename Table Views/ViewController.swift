f6897dfa87e0e7b671e812b05f568c91fe1cb5f1
Jena Grafton <JenaLGrafton@gmail.com>
2015-10-07 12:03:01 -0500

    Added Table views with cell identifier

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 3
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Test"
+        
+        return cell
+        
     }
 
     override func didReceiveMemoryWarning() {
